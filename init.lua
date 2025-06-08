9f5e2b6ae12525cda6b18b8b80f40696feb96fcd
Benjamin Zimmerman <88516145+spectrezim@users.noreply.github.com>
2025-06-08 02:00:34 -0500

    Add obsidian.nvim

--- init.lua
+++ init.lua
@@ -888,6 +888,40 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  { -- Plugin for writing and navigating Obsidian vaults
+    'obsidian-nvim/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- Required
+      'hrsh7th/nvim-cmp', -- Required for link completion
+      'nvim-telescope/telescope.nvim',
+    },
+    ---@module 'obsidian'
+    ---@type obsidian.config.ClientOpts
+    opts = {
+      workspaces = {
+        {
+          name = 'notes',
+          path = '~/Notes',
+        },
+      },
+      completion = {
+        nvim_cmp = true,
+        min_chars = 2,
+      },
+    },
+  },
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
